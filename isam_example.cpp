ffe8bdfcea0c62ffe8c62f154b82147ccad7de8e
guillermoduenasarana <gduenasarana@hotmail.com>
2019-08-01 12:30:37 -0700

    gps added + initial estimates from resutls

--- isam_example.cpp
+++ isam_example.cpp
@@ -1,15 +1,24 @@
+// TODO 
+// corrupts the IMU & gps msmts with noise and use that same noise in the filtering
+// for that i need to figure out how the imu noise is specified: continuous - discrete time conversion.
+
+
 
 #include <gtsam/geometry/SimpleCamera.h>
 #include <gtsam/inference/Symbol.h>
 #include <gtsam/navigation/ImuBias.h>
+#include <gtsam/navigation/CombinedImuFactor.h>
 #include <gtsam/navigation/ImuFactor.h>
+#include <gtsam/navigation/GPSFactor.h>
 #include <gtsam/navigation/Scenario.h>
 #include <gtsam/nonlinear/ISAM2.h>
 #include <gtsam/slam/BetweenFactor.h>
 #include <gtsam/slam/PriorFactor.h>
 #include <typeinfo>
-
+#include <fstream>
 #include <vector>
+#include <random>
+
 
 using namespace std;
 using namespace gtsam;
@@ -23,15 +32,16 @@ const double kGravity = 9.81;
 
 /* ************************************************************************* */
 int main(int argc, char* argv[]) {
-  boost::shared_ptr<PreintegrationParams> imu_params = PreintegrationParams::MakeSharedU(kGravity);
+
+    // IMU preintegrator
+  boost::shared_ptr<PreintegratedCombinedMeasurements::Params> imu_params = 
+                              PreintegratedCombinedMeasurements::Params::MakeSharedU(kGravity);
   imu_params->accelerometerCovariance = (I_3x3 * 0.1);
   imu_params->setGyroscopeCovariance(I_3x3 * 0.1);
   imu_params->setIntegrationCovariance(I_3x3 * 0.1);
   imu_params->setUse2ndOrderCoriolis(false);
   imu_params->setOmegaCoriolis(Vector3(0, 0, 0));
-
-  // IMU preintegrator
-  PreintegratedImuMeasurements accum(imu_params);
+  PreintegratedCombinedMeasurements accum(imu_params);
 
   // I think this is the error that we introduce artificially in the starting points
   Pose3 delta(Rot3::Rodrigues(-0.1, 0.2, 0.25), Point3(0.05, -0.10, 0.20));
@@ -51,7 +61,7 @@ int main(int argc, char* argv[]) {
   ConstantTwistScenario scenario(angular_velocity_vector, linear_velocity_vector, pose_0);
 
   // Create a factor graph
-  NonlinearFactorGraph newgraph;
+  NonlinearFactorGraph newgraph, complete_graph;
 
   // Create (incremental) ISAM2 solver
   ISAM2 isam;
@@ -65,6 +75,7 @@ int main(int argc, char* argv[]) {
   noiseModel::Diagonal::shared_ptr pose_noise = noiseModel::Diagonal::Sigmas((Vector(6) << Vector3::Constant(0.1), Vector3::Constant(0.3)).finished());
   PriorFactor<Pose3> pose_prior(X(0), pose_0, pose_noise);
   newgraph.add(PriorFactor<Pose3>(X(0), pose_0, pose_noise));
+  complete_graph.add(PriorFactor<Pose3>(X(0), pose_0, pose_noise));
   initialEstimate.insert(X(0), pose_0);
 
   // add velocity prior to graph and init values
@@ -73,65 +84,77 @@ int main(int argc, char* argv[]) {
   noiseModel::Diagonal::shared_ptr velnoise = noiseModel::Diagonal::Sigmas(Vector3(0.1, 0.1, 0.1));
   PriorFactor<Vector> vel_prior(V(0), velocity_prior, velnoise);
   newgraph.add(vel_prior);
+  complete_graph.add(vel_prior);  
   initialEstimate.insert(V(0), velocity_prior);
 
-
   // Add bias priors to graph and init values
-  Key biasKey(B(0));
   noiseModel::Diagonal::shared_ptr biasnoise = noiseModel::Diagonal::Sigmas(Vector6::Constant(0.1));
-  PriorFactor<imuBias::ConstantBias> biasprior(biasKey, imuBias::ConstantBias(), biasnoise);
-  newgraph.add(biasprior); // add or push_back seems to be the same
-  initialEstimate.insert(biasKey, imuBias::ConstantBias());
+  PriorFactor<imuBias::ConstantBias> biasprior(B(0), imuBias::ConstantBias(), biasnoise);
+  newgraph.add(biasprior); 
+  complete_graph.add(biasprior); 
+  initialEstimate.insert(B(0), imuBias::ConstantBias());
 
-  // IMU covariance always the same
-  noiseModel::Diagonal::shared_ptr imu_cov = 
-          noiseModel::Diagonal::Variances( (Vector(6) << 0.1, 0.1, 0.1, 0.1, 0.1, 0.1).finished() );
+  // solve the graph once
+  isam.update(newgraph, initialEstimate);
+  result = isam.calculateEstimate();
+  newgraph = NonlinearFactorGraph();
+  initialEstimate.clear();
+
+   // noise generator
+  std::default_random_engine noise_generator;
+  std::normal_distribution<double> distribution(5.0,2.0);
+
+  // initialize variables
+  noiseModel::Diagonal::shared_ptr gps_cov = noiseModel::Isotropic::Sigma(3,1.0); // GPS covariance is constant
+  NavState prev_state, predict_state;
+  imuBias::ConstantBias prev_bias;
 
   // Simulate poses and imu measurements, adding them to the factor graph
-  for (size_t i = 0; i < 36; ++i) {
-    double t = i * delta_t;
-    if (i == 0) {  // First time add two poses
-      Pose3 pose_1 = scenario.pose(delta_t);
-      // initialEstimate.insert(X(0), pose_0.compose(delta)); // I think it's adding delta to make the initial estimate incorrect
-      initialEstimate.insert(X(1), pose_1.compose(delta));
-    } else if (i >= 2) {  // Add more poses as necessary
-      Pose3 pose_i = scenario.pose(t);
-      initialEstimate.insert(X(i), pose_i.compose(delta));
-    }
+  for (size_t i = 1; i < 10; ++i) {
 
-    if (i > 0) {
-      // Add Bias variables periodically
-      if (i % 5 == 0) {
-        biasKey++;
-        Symbol b1 = biasKey - 1;
-        Symbol b2 = biasKey;
+    double current_time = i * delta_t;
+      
+    // Predict acceleration and gyro measurements in (actual) body frame
+    Vector3 measuredAcc = scenario.acceleration_b(current_time) -
+                          scenario.rotation(current_time).transpose() * imu_params->n_gravity;
+    Vector3 measuredOmega = scenario.omega_b(current_time);
+    accum.integrateMeasurement(measuredAcc, measuredOmega, delta_t);
+    prev_state = NavState(result.at<Pose3>(X(i-1)), result.at<Vector3>(V(i-1)));
+    prev_bias = result.at<imuBias::ConstantBias>(B(i-1));
+    predict_state = accum.predict(prev_state, prev_bias);
 
-        newgraph.emplace_shared<BetweenFactor<imuBias::ConstantBias> >(b1, b2, imuBias::ConstantBias(), imu_cov);
-        initialEstimate.insert(biasKey, imuBias::ConstantBias());
-      }
-      // Predict acceleration and gyro measurements in (actual) body frame
-      Vector3 measuredAcc = scenario.acceleration_b(t) -
-                            scenario.rotation(t).transpose() * imu_params->n_gravity;
-      Vector3 measuredOmega = scenario.omega_b(t);
-      accum.integrateMeasurement(measuredAcc, measuredOmega, delta_t);
+    // insert init values
+    Pose3 pose_i = scenario.pose(current_time);
+    initialEstimate.insert(X(i), predict_state.pose());
+    initialEstimate.insert(V(i), predict_state.velocity());
+    initialEstimate.insert(B(i), imuBias::ConstantBias());  
 
-      // Add Imu Factor
-      ImuFactor imufac(X(i - 1), V(i - 1), X(i), V(i), biasKey, accum);
-      newgraph.add(imufac);
+    // Add Imu Factor
+    CombinedImuFactor imufac(X(i - 1), V(i - 1), X(i), V(i), B(i - 1), B(i), accum);
+    newgraph.add(imufac);
+    complete_graph.add(imufac);
 
-      // insert new velocity, which is wrong
-      initialEstimate.insert(V(i), velocity_prior);
-      accum.resetIntegration();
-    }
+    // Adding GPS factor
+    GPSFactor gps_factor(X(i), scenario.pose(current_time).translation(), gps_cov);
+    newgraph.add(gps_factor);
+    complete_graph.add(gps_factor);  
 
     // Incremental solution
     isam.update(newgraph, initialEstimate);
     result = isam.calculateEstimate();
     newgraph = NonlinearFactorGraph();
+    accum.resetIntegration();
     initialEstimate.clear();
   } // end for loop
   
-  GTSAM_PRINT(result);
+  // save factor graph as graphviz dot file
+  ofstream os("isam_example.dot");
+  complete_graph.saveGraph(os, result);
+  
+  // Also print out to console
+  // newgraph.saveGraph(cout, result);
+
+  // GTSAM_PRINT(result);
   return 0;
 }
 /* ************************************************************************* */
