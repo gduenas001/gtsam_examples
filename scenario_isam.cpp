9e5be1a731e4350e88108e30921886ffd5e79a76
guillermoduenasarana <gduenasarana@hotmail.com>
2019-08-03 18:47:28 -0700

    clear code for first PR

--- scenario_isam.cpp
+++ scenario_isam.cpp
@@ -1,7 +1,6 @@
 // TODO 
-// corrupts the IMU & gps msmts with noise and use that same noise in the filtering
-// for that i need to figure out how the imu noise is specified: continuous - discrete time conversion.
-// compare the estimate with the actual scenario pose in plots
+// - need to figure out how the imu noise is specified: continuous - discrete time conversion.
+// - add lidar msmts to landmarks (start with known associations)
 
 
 #include <gtsam/geometry/SimpleCamera.h>
@@ -25,8 +24,8 @@ using namespace std;
 using namespace gtsam;
 
 // Shorthand for velocity and pose variables
-using symbol_shorthand::V;
 using symbol_shorthand::X;
+using symbol_shorthand::V;
 using symbol_shorthand::B;
 
 const double kGravity = 9.81;
@@ -36,11 +35,38 @@ Point3 generate_random_point(std::default_random_engine &generator, std::normal_
   return Point3(distribution(generator),distribution(generator),distribution(generator));
 }
 
+// add a noiseless prior factor
+void addNoiselessPriorFactor(NonlinearFactorGraph &new_graph, NonlinearFactorGraph &complete_graph, Values &initial_estimate,
+                             const Scenario &scenario) {
+  // Add a prior on pose x0. This indirectly specifies where the origin is.
+  noiseModel::Diagonal::shared_ptr pose_noise = noiseModel::Diagonal::Sigmas((Vector(6) << Vector3::Constant(0.0), Vector3::Constant(0.0)).finished());
+  PriorFactor<Pose3> pose_prior(X(0), scenario.pose(0), pose_noise);
+  new_graph.add(PriorFactor<Pose3>(X(0), scenario.pose(0), pose_noise));
+  complete_graph.add(PriorFactor<Pose3>(X(0), scenario.pose(0), pose_noise));
+  initial_estimate.insert(X(0), scenario.pose(0));
+
+  // add velocity prior to graph and init values
+  Vector vel_prior(3); // needs to be a dynamically allocated vector (I don't know why)
+  vel_prior= scenario.velocity_n(0);
+  noiseModel::Diagonal::shared_ptr vel_noise = noiseModel::Diagonal::Sigmas( Vector3::Constant(0.0) ); // default 0.01
+  PriorFactor<Vector> vel_prior_factor(V(0), vel_prior, vel_noise);
+  new_graph.add(vel_prior_factor);
+  complete_graph.add(vel_prior_factor);  
+  initial_estimate.insert(V(0), vel_prior);
+
+  // Add bias priors to graph and init values
+  noiseModel::Diagonal::shared_ptr bias_noise = noiseModel::Diagonal::Sigmas(Vector6::Constant(0.0)); // default 0.1
+  PriorFactor<imuBias::ConstantBias> bias_prior_factor(B(0), imuBias::ConstantBias(), bias_noise);
+  new_graph.add(bias_prior_factor); 
+  complete_graph.add(bias_prior_factor); 
+  initial_estimate.insert(B(0), imuBias::ConstantBias());
+
+}
 
 /* ************************************************************************* */
 int main(int argc, char* argv[]) {
 
-  // parameters
+  // run time options
   float sim_time;
   if (argc == 2){
     sim_time= atof(argv[1]);
@@ -48,20 +74,23 @@ int main(int argc, char* argv[]) {
     sim_time= 30; // seconds
   }
 
-  // IMU preintegrator
+  // input parameters
   double accel_noise_sigma = 0.1; // what units?? 0003924
   double gyro_noise_sigma = 0.1; //000205689024915
-  double accel_bias_rw_sigma = 1e-20; //004905
-  double gyro_bias_rw_sigma = 1e-20; //000001454441043
+  double accel_bias_rw_sigma = 0.005; //1e-20; //004905
+  double gyro_bias_rw_sigma = 0.000001454441043; //1e-20; //000001454441043
   double gps_noise_sigma = 3; // meters
-  Matrix33 measured_acc_cov = I_3x3 * pow(accel_noise_sigma,2);
-  Matrix33 measured_omega_cov = I_3x3 * pow(gyro_noise_sigma,2);
-  Matrix33 integration_error_cov = I_3x3 * 1e-20; // error committed in integrating position from velocities, 8
-  Matrix33 bias_acc_cov = I_3x3 * pow(accel_bias_rw_sigma,2);
-  Matrix33 bias_omega_cov = I_3x3 * pow(gyro_bias_rw_sigma,2);
-  Matrix66 bias_acc_omega_int = Matrix::Identity(6,6) * 1e-20; // error in the bias used for preintegration, 5
+  double dt_imu = 1.0 / 125, // makes for 10 degrees per step (1.0 / 18)
+         dt_gps = 1.0; // seconds
 
   // create parameters
+  Matrix33 measured_acc_cov = I_3x3 * pow(accel_noise_sigma,2);
+  Matrix33 measured_omega_cov = I_3x3 * pow(gyro_noise_sigma,2);
+  Matrix33 bias_acc_cov = I_3x3 * pow(accel_bias_rw_sigma,2);
+  Matrix33 bias_omega_cov = I_3x3 * pow(gyro_bias_rw_sigma,2);
+  Matrix33 integration_error_cov = I_3x3 * 1e-20; // error committed in integrating position from velocities, 8 
+  Matrix66 bias_acc_omega_int = Matrix::Identity(6,6) * 1e-20; // error in the bias used for preintegration, 5
+
   boost::shared_ptr<PreintegratedCombinedMeasurements::Params> imu_params = 
                               PreintegratedCombinedMeasurements::Params::MakeSharedU(kGravity);
   imu_params->setAccelerometerCovariance(measured_acc_cov);
@@ -73,6 +102,11 @@ int main(int argc, char* argv[]) {
   imu_params->setUse2ndOrderCoriolis(false);
   imu_params->setOmegaCoriolis(Vector3(0, 0, 0));
   PreintegratedCombinedMeasurements accum(imu_params);
+  std::default_random_engine noise_generator;    // noise generator
+  std::normal_distribution<double> accel_noise_dist(0, accel_noise_sigma);
+  std::normal_distribution<double> gyro_noise_dist(0, gyro_noise_sigma);
+  std::normal_distribution<double> gps_noise_dist(0, gps_noise_sigma);
+  noiseModel::Diagonal::shared_ptr gps_cov = noiseModel::Isotropic::Sigma(3, gps_noise_sigma); // GPS covariance is constant
 
   // Start with a camera on x-axis looking at origin (only use pose_0 from here to generate the scenario)
   double radius = 30;
@@ -82,9 +116,7 @@ int main(int argc, char* argv[]) {
   const Pose3 pose_0 = camera.pose();
 
   // Now, create a constant-twist scenario that makes the camera orbit the origin
-  double angular_velocity = M_PI/4,  // rad/sec
-         dt_imu = 1.0 / 125, // makes for 10 degrees per step (1.0 / 18)
-         dt_gps = 1.0;
+  double angular_velocity = M_PI/4;  // rad/sec
   Vector3 angular_velocity_vector(0, -angular_velocity, 0);
   Vector3 linear_velocity_vector(radius * angular_velocity, 0, 0);
   ConstantTwistScenario scenario(angular_velocity_vector, linear_velocity_vector, pose_0);
@@ -92,56 +124,24 @@ int main(int argc, char* argv[]) {
   // Create a factor graph &  ISAM2
   NonlinearFactorGraph newgraph, complete_graph;
   ISAM2 isam;
-
-  // Create the initial estimate to the solution
-  Values initialEstimate, totalEstimate, result;
-
-  // Add a prior on pose x0. This indirectly specifies where the origin is.
-  // 0.1 rad std on roll, pitch, yaw, 30cm std on x,y,z.
-  noiseModel::Diagonal::shared_ptr pose_noise = noiseModel::Diagonal::Sigmas((Vector(6) << Vector3::Constant(0.0), Vector3::Constant(0.0)).finished());
-  PriorFactor<Pose3> pose_prior(X(0), pose_0, pose_noise);
-  newgraph.add(PriorFactor<Pose3>(X(0), pose_0, pose_noise));
-  complete_graph.add(PriorFactor<Pose3>(X(0), pose_0, pose_noise));
-  initialEstimate.insert(X(0), pose_0);
-
-  // add velocity prior to graph and init values
-  Vector vel_prior(3); // needs to be a dynamically allocated vector (I don't know why)
-  vel_prior= scenario.velocity_n(0);
-  noiseModel::Diagonal::shared_ptr vel_noise = noiseModel::Diagonal::Sigmas( Vector3::Constant(0.0) ); // default 0.01
-  PriorFactor<Vector> vel_prior_factor(V(0), vel_prior, vel_noise);
-  newgraph.add(vel_prior_factor);
-  complete_graph.add(vel_prior_factor);  
-  initialEstimate.insert(V(0), vel_prior);
-
-  // Add bias priors to graph and init values
-  noiseModel::Diagonal::shared_ptr bias_noise = noiseModel::Diagonal::Sigmas(Vector6::Constant(0.0)); // default 0.1
-  PriorFactor<imuBias::ConstantBias> bias_prior_factor(B(0), imuBias::ConstantBias(), bias_noise);
-  newgraph.add(bias_prior_factor); 
-  complete_graph.add(bias_prior_factor); 
-  initialEstimate.insert(B(0), imuBias::ConstantBias());
-
-  // solve the graph once
-  isam.update(newgraph, initialEstimate);
-  result = isam.calculateEstimate();
-  newgraph = NonlinearFactorGraph();
-  initialEstimate.clear();
-
-   // noise generator
-  std::default_random_engine noise_generator;
-  std::normal_distribution<double> accel_noise_dist(0, accel_noise_sigma);
-  std::normal_distribution<double> gyro_noise_dist(0, gyro_noise_sigma);
-  std::normal_distribution<double> gps_noise_dist(0, gps_noise_sigma);
+  Values initialEstimate, result; // Create the initial estimate to the solution
 
   // initialize variables
   double gps_time_accum = 0.0,
          current_time = 0.0,
          num_imu_epochs = sim_time / dt_imu;
-  int pose_factor_count = 0;
-  noiseModel::Diagonal::shared_ptr gps_cov = noiseModel::Isotropic::Sigma(3, gps_noise_sigma); // GPS covariance is constant
+  int pose_factor_count = 1;
   NavState prev_state, predict_state;
   imuBias::ConstantBias prev_bias;
   std::vector<Point3> true_positions;
   true_positions.push_back( scenario.pose(0).translation() );
+
+  // solve the graph once
+  addNoiselessPriorFactor(newgraph, complete_graph, initialEstimate, scenario);
+  isam.update(newgraph, initialEstimate);
+  result = isam.calculateEstimate();
+  newgraph = NonlinearFactorGraph();
+  initialEstimate.clear();  
 
   // Simulate poses and imu measurements, adding them to the factor graph
   for (size_t i = 1; i < num_imu_epochs; ++i) {
@@ -163,10 +163,7 @@ int main(int argc, char* argv[]) {
 
     // GPS update 
     if (gps_time_accum > dt_gps) {
-      // increase the factor count
-      pose_factor_count++; 
 
-      // 
       prev_state = NavState(result.at<Pose3>(X(pose_factor_count-1)), result.at<Vector3>(V(pose_factor_count-1)));
       prev_bias = result.at<imuBias::ConstantBias>(B(pose_factor_count-1));
       predict_state = accum.predict(prev_state, prev_bias);
@@ -198,6 +195,7 @@ int main(int argc, char* argv[]) {
       accum.resetIntegration();
       initialEstimate.clear();
       gps_time_accum = 0.0;
+      pose_factor_count++;  // increase the factor count
     }
   } // end for loop
   
