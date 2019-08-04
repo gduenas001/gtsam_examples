fa75f18bf1943de1271d3011a850baae41fe818d
guillermoduenasarana <gduenasarana@hotmail.com>
2019-08-03 18:10:36 -0700

    gps updates less frequent

--- isam_example.cpp
+++ isam_example.cpp
@@ -33,12 +33,7 @@ const double kGravity = 9.81;
 
 // generate a random 3D point
 Point3 generate_random_point(std::default_random_engine &generator, std::normal_distribution<double> &distribution) {
-  Point3 point(distribution(generator),distribution(generator),distribution(generator));
-  // point.x = distribution(generator);
-  // point.y = distribution(generator);
-  // point.z = distribution(generator);
-
-  return point;
+  return Point3(distribution(generator),distribution(generator),distribution(generator));
 }
 
 
@@ -46,25 +41,25 @@ Point3 generate_random_point(std::default_random_engine &generator, std::normal_
 int main(int argc, char* argv[]) {
 
   // parameters
-  int num_imu_epochs;
+  float sim_time;
   if (argc == 2){
-    num_imu_epochs= atoi(argv[1]);
+    sim_time= atof(argv[1]);
   } else {
-    num_imu_epochs= 100;
+    sim_time= 30; // seconds
   }
 
   // IMU preintegrator
-  double accel_noise_sigma = 0.0003924; // what units??
-  double gyro_noise_sigma = 0.000205689024915;
-  double accel_bias_rw_sigma = 0.004905;
-  double gyro_bias_rw_sigma = 0.000001454441043;
-  double gps_noise_sigma = 0.5; // meters
+  double accel_noise_sigma = 0.1; // what units?? 0003924
+  double gyro_noise_sigma = 0.1; //000205689024915
+  double accel_bias_rw_sigma = 1e-20; //004905
+  double gyro_bias_rw_sigma = 1e-20; //000001454441043
+  double gps_noise_sigma = 3; // meters
   Matrix33 measured_acc_cov = I_3x3 * pow(accel_noise_sigma,2);
   Matrix33 measured_omega_cov = I_3x3 * pow(gyro_noise_sigma,2);
-  Matrix33 integration_error_cov = I_3x3 * 1e-8; // error committed in integrating position from velocities
+  Matrix33 integration_error_cov = I_3x3 * 1e-20; // error committed in integrating position from velocities, 8
   Matrix33 bias_acc_cov = I_3x3 * pow(accel_bias_rw_sigma,2);
   Matrix33 bias_omega_cov = I_3x3 * pow(gyro_bias_rw_sigma,2);
-  Matrix66 bias_acc_omega_int = Matrix::Identity(6,6) * 1e-5; // error in the bias used for preintegration
+  Matrix66 bias_acc_omega_int = Matrix::Identity(6,6) * 1e-20; // error in the bias used for preintegration, 5
 
   // create parameters
   boost::shared_ptr<PreintegratedCombinedMeasurements::Params> imu_params = 
@@ -87,8 +82,9 @@ int main(int argc, char* argv[]) {
   const Pose3 pose_0 = camera.pose();
 
   // Now, create a constant-twist scenario that makes the camera orbit the origin
-  double angular_velocity = M_PI,  // rad/sec
-         delta_t = 1.0 / 18;          // makes for 10 degrees per step
+  double angular_velocity = M_PI/4,  // rad/sec
+         dt_imu = 1.0 / 125, // makes for 10 degrees per step (1.0 / 18)
+         dt_gps = 1.0;
   Vector3 angular_velocity_vector(0, -angular_velocity, 0);
   Vector3 linear_velocity_vector(radius * angular_velocity, 0, 0);
   ConstantTwistScenario scenario(angular_velocity_vector, linear_velocity_vector, pose_0);
@@ -102,7 +98,7 @@ int main(int argc, char* argv[]) {
 
   // Add a prior on pose x0. This indirectly specifies where the origin is.
   // 0.1 rad std on roll, pitch, yaw, 30cm std on x,y,z.
-  noiseModel::Diagonal::shared_ptr pose_noise = noiseModel::Diagonal::Sigmas((Vector(6) << Vector3::Constant(0.1), Vector3::Constant(0.3)).finished());
+  noiseModel::Diagonal::shared_ptr pose_noise = noiseModel::Diagonal::Sigmas((Vector(6) << Vector3::Constant(0.0), Vector3::Constant(0.0)).finished());
   PriorFactor<Pose3> pose_prior(X(0), pose_0, pose_noise);
   newgraph.add(PriorFactor<Pose3>(X(0), pose_0, pose_noise));
   complete_graph.add(PriorFactor<Pose3>(X(0), pose_0, pose_noise));
@@ -110,17 +106,15 @@ int main(int argc, char* argv[]) {
 
   // add velocity prior to graph and init values
   Vector vel_prior(3); // needs to be a dynamically allocated vector (I don't know why)
-  // vel_prior << 0, angular_velocity * radius, 0;
-  // cout<< "initial velocity "<< scenario.velocity_n(0)<< endl;
   vel_prior= scenario.velocity_n(0);
-  noiseModel::Diagonal::shared_ptr vel_noise = noiseModel::Diagonal::Sigmas( Vector3::Constant(0.01) );
+  noiseModel::Diagonal::shared_ptr vel_noise = noiseModel::Diagonal::Sigmas( Vector3::Constant(0.0) ); // default 0.01
   PriorFactor<Vector> vel_prior_factor(V(0), vel_prior, vel_noise);
   newgraph.add(vel_prior_factor);
   complete_graph.add(vel_prior_factor);  
   initialEstimate.insert(V(0), vel_prior);
 
   // Add bias priors to graph and init values
-  noiseModel::Diagonal::shared_ptr bias_noise = noiseModel::Diagonal::Sigmas(Vector6::Constant(0.1));
+  noiseModel::Diagonal::shared_ptr bias_noise = noiseModel::Diagonal::Sigmas(Vector6::Constant(0.0)); // default 0.1
   PriorFactor<imuBias::ConstantBias> bias_prior_factor(B(0), imuBias::ConstantBias(), bias_noise);
   newgraph.add(bias_prior_factor); 
   complete_graph.add(bias_prior_factor); 
@@ -139,6 +133,10 @@ int main(int argc, char* argv[]) {
   std::normal_distribution<double> gps_noise_dist(0, gps_noise_sigma);
 
   // initialize variables
+  double gps_time_accum = 0.0,
+         current_time = 0.0,
+         num_imu_epochs = sim_time / dt_imu;
+  int pose_factor_count = 0;
   noiseModel::Diagonal::shared_ptr gps_cov = noiseModel::Isotropic::Sigma(3, gps_noise_sigma); // GPS covariance is constant
   NavState prev_state, predict_state;
   imuBias::ConstantBias prev_bias;
@@ -148,60 +146,62 @@ int main(int argc, char* argv[]) {
   // Simulate poses and imu measurements, adding them to the factor graph
   for (size_t i = 1; i < num_imu_epochs; ++i) {
 
-    double current_time = i * delta_t;
+    current_time = i * dt_imu;
+    gps_time_accum += dt_imu;
       
     // save the current position
     true_positions.push_back( scenario.pose(current_time).translation() );
 
     // Predict acceleration and gyro measurements in (actual) body frame
+    Point3 acc_noise = generate_random_point( noise_generator, accel_noise_dist );
     Vector3 measuredAcc = scenario.acceleration_b(current_time) -
                           scenario.rotation(current_time).transpose() * imu_params->n_gravity;
-    Vector3 measuredOmega = scenario.omega_b(current_time);
-    accum.integrateMeasurement(measuredAcc, measuredOmega, delta_t);
-    prev_state = NavState(result.at<Pose3>(X(i-1)), result.at<Vector3>(V(i-1)));
-    prev_bias = result.at<imuBias::ConstantBias>(B(i-1));
-    predict_state = accum.predict(prev_state, prev_bias);
+                          // acc_noise.vector();
+    Point3 gyro_noise = generate_random_point( noise_generator, gyro_noise_dist );                      
+    Vector3 measuredOmega = scenario.omega_b(current_time); // + gyro_noise.vector();
+    accum.integrateMeasurement(measuredAcc, measuredOmega, dt_imu);
 
-    // Known init values
-    Pose3 pose_i = scenario.pose(current_time);
-    initialEstimate.insert(X(i), scenario.pose(current_time) );
-    initialEstimate.insert(V(i), scenario.velocity_n(current_time) );
-    initialEstimate.insert(B(i), imuBias::ConstantBias() );
+    // GPS update 
+    if (gps_time_accum > dt_gps) {
+      // increase the factor count
+      pose_factor_count++; 
 
-    // // predicted init values
-    // initialEstimate.insert(X(i), predict_state.pose());
-    // initialEstimate.insert(V(i), predict_state.velocity());
-    // initialEstimate.insert(B(i), imuBias::ConstantBias());  
+      // 
+      prev_state = NavState(result.at<Pose3>(X(pose_factor_count-1)), result.at<Vector3>(V(pose_factor_count-1)));
+      prev_bias = result.at<imuBias::ConstantBias>(B(pose_factor_count-1));
+      predict_state = accum.predict(prev_state, prev_bias);
 
-    // Add Imu Factor
-    CombinedImuFactor imufac(X(i - 1), V(i - 1), X(i), V(i), B(i - 1), B(i), accum);
-    newgraph.add(imufac);
-    complete_graph.add(imufac);
+      // predicted init values
+      initialEstimate.insert(X(pose_factor_count), predict_state.pose());
+      initialEstimate.insert(V(pose_factor_count), predict_state.velocity());
+      initialEstimate.insert(B(pose_factor_count), imuBias::ConstantBias());  
 
-    // // Adding GPS factor
-    if (i < 6) {
-      Point3 point_noise(generate_random_point(noise_generator, gps_noise_dist));
-      // Point3 point_noise(Point3(gps_noise_dist(noise_generator),gps_noise_dist(noise_generator),gps_noise_dist(noise_generator)));
-      Point3 gps_msmt = scenario.pose(current_time).translation() + 
-        point_noise;
-        // Point3(gps_noise_dist(noise_generator),gps_noise_dist(noise_generator),gps_noise_dist(noise_generator));
-       // generate_random_point(noise_generator, gps_noise_dist);
+      // Add Imu Factor
+      CombinedImuFactor imufac(X(pose_factor_count - 1), V(pose_factor_count - 1), 
+                               X(pose_factor_count),     V(pose_factor_count), 
+                               B(pose_factor_count - 1), B(pose_factor_count), accum);
+      newgraph.add(imufac);
+      complete_graph.add(imufac);
 
-      cout<< "generated random point for gps noise: "<< point_noise<< endl;
-
-      GPSFactor gps_factor(X(i), gps_msmt, gps_cov);
+      // // Adding GPS factor
+      Point3 gps_msmt = scenario.pose(current_time).translation() + Point3(generate_random_point(noise_generator, gps_noise_dist));
+      GPSFactor gps_factor(X(pose_factor_count), gps_msmt, gps_cov);
       newgraph.add(gps_factor);
       complete_graph.add(gps_factor);  
+      
+      // Incremental solution
+      isam.update(newgraph, initialEstimate);
+      result = isam.calculateEstimate();
+
+      // reset variables
+      newgraph = NonlinearFactorGraph();
+      accum.resetIntegration();
+      initialEstimate.clear();
+      gps_time_accum = 0.0;
     }
-    
-    // Incremental solution
-    isam.update(newgraph, initialEstimate);
-    result = isam.calculateEstimate();
-    newgraph = NonlinearFactorGraph();
-    accum.resetIntegration();
-    initialEstimate.clear();
   } // end for loop
   
+
   // write poses into file
   string filename = "estimated_positions.csv";
   fstream stream(filename.c_str(), fstream::out);
@@ -229,7 +229,7 @@ int main(int argc, char* argv[]) {
   // ofstream os("isam_example.dot");
   // complete_graph.saveGraph(os, result);
 
-  GTSAM_PRINT(result);
+  // GTSAM_PRINT(result);
 
 
   return 0;
